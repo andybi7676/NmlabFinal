d967a50eaf80df6c7d8c2416c423d3276416ec2b
andybi7676 <59821107+andybi7676@users.noreply.github.com>
2020-12-29 00:27:41 +0800

    frontend update

--- client/src/components/Map.js
+++ client/src/components/Map.js
@@ -1,26 +1,31 @@
 import React from "react";
 import Cell from './Cell';
 import "../styles/Map.css";
+import mp_img from "../images/background.jpg";
+import test_png from '../images/test_div.png';
+import { Button } from "semantic-ui-react";
+import Draggable from 'react-draggable';
 
 
 const Map = () => {
   const row = 8;
   const col = 8;
   const usrMap = new Array(row).fill(0).map(() => new Array(col).fill(0).map((_,idx) => idx));
+  const cellAry = [[10, 10], [130, 50], [380, 270]];
   // console.log(usrMap);
   return (
-    <div className="map">
-      {
-        usrMap.map((ary, row) => (
-          <div key={row} className="map_row">
-            {
-              ary.map((col) => (
-                <Cell key={col} x={row} y={col} />
-              ))
-            }
-          </div>
-        ))
-      }
+    <div className="flex_div" >
+      <Draggable>
+        <div className="inline_div">
+          <img src={test_png} alt="Workplace" className="background_img"/>
+          {
+            cellAry.map((xy_list, idx) => {
+              const [x, y] = xy_list;
+              return <Cell key={idx} x={x} y={y} />
+            })
+          }
+        </div>
+      </Draggable>
     </div>
   );
 }
