94698fa6908dcf918a1d11ac2dce72fd9ea570d4
hardness1020 <65449789+hardness1020@users.noreply.github.com>
2020-12-31 15:34:17 +0800

    update attack

--- contracts/Attack.sol
+++ contracts/Attack.sol
@@ -6,41 +6,53 @@ import "./SafeMath.sol";
 
 contract Attack is Account, Soldier {
 
-    function _fight(address winner, address loser) internal{
-        int powerGap = int(power[loser]/2);
-        while (powerGap >= 0){
+    function _fight(address myCastle, address attackedCastle) internal{
+        address winner;
+        address loser;
+
+        if (power[myCastle] > power[attackedCastle]){
+            winner = myCastle;
+            loser = attackedCastle;
+
+            foodOwnerCount[winner].add(foodOwnerCount[loser] * 4/5);
+            woodOwnerCount[winner].add(woodOwnerCount[loser] * 4/5);
+            ironOwnerCount[winner].add(ironOwnerCount[loser] * 4/5);
+            stoneOwnerCount[winner].add(stoneOwnerCount[loser] * 4/5);
+            coinOwnerCount[winner].add(coinOwnerCount[loser] * 4/5);
+            foodOwnerCount[loser].sub(foodOwnerCount[loser] * 4/5);
+            woodOwnerCount[loser].sub(woodOwnerCount[loser] * 4/5);
+            ironOwnerCount[loser].sub(ironOwnerCount[loser] * 4/5);
+            stoneOwnerCount[loser].sub(stoneOwnerCount[loser] * 4/5);
+            coinOwnerCount[loser].sub(coinOwnerCount[loser] * 4/5);
+        }
+        else {
+            winner = attackedCastle;
+            loser = myCastle;
+        }
+
+        int winnerPowerLose = int(power[loser] / 2);
+        while (winnerPowerLose >= 0){
             if(numOfArcher[winner] > 0){
                 numOfArcher[winner]--;
-                powerGap -= levelOfArcher[winner];
+                winnerPowerLose -= levelOfArcher[winner];
             }
             if(numOfCavalry[winner] > 0){
                 numOfCavalry[winner]--;
-                powerGap -= levelOfCavalry[winner];
+                winnerPowerLose -= levelOfCavalry[winner];
             }
             if(numOfInfantry[winner] > 0){
                 numOfInfantry[winner]--;
-                powerGap -= levelOfInfantry[winner];
+                winnerPowerLose -= levelOfInfantry[winner];
             }
             if(numOfPikemen[winner] > 0){
                 numOfPikemen[winner]--;
-                powerGap -= levelOfPikemen[winner];
+                winnerPowerLose -= levelOfPikemen[winner];
             }
         }
         numOfArcher[loser].sub(numOfArcher[loser] * 4/5);
         numOfCavalry[loser].sub(numOfCavalry[loser] * 4/5);
         numOfInfantry[loser].sub(numOfInfantry[loser] * 4/5);
         numOfPikemen[loser].sub(numOfPikemen[loser] * 4/5);
-
-        foodOwnerCount[winner].add(foodOwnerCount[loser] * 4/5);
-        woodOwnerCount[winner].add(woodOwnerCount[loser] * 4/5);
-        ironOwnerCount[winner].add(ironOwnerCount[loser] * 4/5);
-        stoneOwnerCount[winner].add(stoneOwnerCount[loser] * 4/5);
-        coinOwnerCount[winner].add(coinOwnerCount[loser] * 4/5);
-        foodOwnerCount[loser].sub(foodOwnerCount[loser] * 4/5);
-        woodOwnerCount[loser].sub(woodOwnerCount[loser] * 4/5);
-        ironOwnerCount[loser].sub(ironOwnerCount[loser] * 4/5);
-        stoneOwnerCount[loser].sub(stoneOwnerCount[loser] * 4/5);
-        coinOwnerCount[loser].sub(coinOwnerCount[loser] * 4/5);
 
         _updatePower(winner);
         _updatePower(loser);
@@ -50,13 +62,7 @@ contract Attack is Account, Soldier {
         require(msg.sender == castleToOwner[_ownerId]);
         address myCastle = castleToOwner[_ownerId];
         address attackedCastle = castleToOwner[_attackedCastleId];
-
-        if (power[myCastle] > power[attackedCastle]){
-            _fight(myCastle, attackedCastle);
-        }
-        else{
-            _fight(attackedCastle, myCastle);
-        }
+        _fight(myCastle, attackedCastle);
     }
 
 }
